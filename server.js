38c8f32d94cd055e3ca2dc70d0371938aed1cdbb
TECH-KASHIF <104705313+TECH-KASHIF@users.noreply.github.com>
2025-04-27 01:18:32 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Homepage
+// Basic route
 app.get('/', (req, res) => {
-  res.send('All-in-One Downloader Server Running!');
+  res.send('<h1>All-in-One Downloader Server Running Successfully!</h1>');
 });
 
 // Start server
 app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });
