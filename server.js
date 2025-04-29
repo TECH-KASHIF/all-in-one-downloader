36814de98e1f4b183985d19c68c27472edf0e6e3
TECH-KASHIF <104705313+TECH-KASHIF@users.noreply.github.com>
2025-04-29 22:54:02 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,35 +1,3 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const port = process.env.PORT || 3000;
-
-// Middleware
-app.use(express.json());
-
-// Serve static files (public folder)
-app.use(express.static(path.join(__dirname, 'public')));
-
-// API Routes
-app.post('/download/instagram', async (req, res) => {
-    const { url } = req.body;
-    // Yahan tumhara Instagram download logic aayega
-    res.json({ url: url, message: "Instagram download success!" });
-});
-
-app.post('/download/facebook', async (req, res) => {
-    const { url } = req.body;
-    // Yahan tumhara Facebook download logic aayega
-    res.json({ url: url, message: "Facebook download success!" });
-});
-
-// Default Route - frontend serve
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
 const axios = require('axios');
 const cheerio = require('cheerio');
 
